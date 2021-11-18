a208855c2b78f97a488972bfb3255cb4d2d654c9
John Adams <84754405+johnadams78@users.noreply.github.com>
2021-11-17 20:03:06 -0500

    third instance added

--- ec2.tf
+++ ec2.tf
@@ -24,21 +24,6 @@ resource "aws_instance" "web" {
 }
 
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
 
 resource "aws_instance" "web2" {
   ami           = data.aws_ami.ubuntu.id
@@ -46,5 +31,14 @@ resource "aws_instance" "web2" {
 
   tags = {
     Name = "HelloWorld2"
+  }
+}
+
+resource "aws_instance" "web3" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld3"
   }
 }
